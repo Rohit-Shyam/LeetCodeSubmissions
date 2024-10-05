6e606130ca9071b2fbbf2aa67fe621208578d7e9
Rohit Shyam <76948811+Rohit-Shyam@users.noreply.github.com>
2024-10-05 13:24:07 -0500

    Time: 9 ms (6.13%) | Memory: 42.4 MB (26.63%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,32 +1,31 @@
-import java.util.HashSet;
 class Solution {
+    public static boolean replyBack(String str) {
+    for (int i = 0; i < str.length(); i++) {
+        for (int j = i + 1; j < str.length(); j++) {
+            if (str.charAt(i) == str.charAt(j)) {
+                return false;  
+            }
+        }
+    }
+    return true;  
+    }
     public int countGoodSubstrings(String s) {
+        int sl = 3;
         int n = s.length();
-        int sl = 3; 
-        int l = 0;
-        String temp = "";
         int ans = 0;
-        for (int r = 0; r < n; r++) {
-            temp = temp + s.charAt(r); 
-            if (r - l == sl) {
-                temp = temp.substring(1); 
-                l++; 
-            }
-            if (r - l + 1 == sl) { 
-                if (hasUniqueCharacters(temp)) { 
-                    ans++; 
+        for(int i =0; i<n; i++){
+            int j = i+ (sl-1);
+                if(j>=n){
+                    break;
                 }
-            }
+                String temp = "";
+                for(int k =i; k<=j;k++){
+                    temp = temp + s.charAt(k);
+                }
+                if(replyBack(temp)){
+                    ans++;
+                }
         }
         return ans;
-    }
-    public static boolean hasUniqueCharacters(String str) {
-        HashSet<Character> charSet = new HashSet<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (!charSet.add(str.charAt(i))) {
-                return false; 
-            }
-        }
-        return true; 
     }
 }
