1005b456010eb61cc610fa420662599a85e893e3
Rohit Shyam <76948811+Rohit-Shyam@users.noreply.github.com>
2024-10-05 20:36:55 -0500

    Time: 3 ms (37.94%) | Memory: 41.6 MB (53.98%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,24 +1,21 @@
 class Solution {
-    public static boolean reply(String s, int sl){
-        HashSet<Character> hs = new HashSet<>();
-        for(int i =0 ;i<s.length();i++){
-            char ch = s.charAt(i);
-            hs.add(ch);
-        }
-        if(hs.size()==sl){
-            return true;
-        }else{
-            return false;
-        }
-    }
-        
     public int countGoodSubstrings(String s) {
-        int n = s.length();
-        int sl = 3;
+        HashMap <Character, Integer> hm = new HashMap<>();
+        int l = 0;
         int ans = 0;
-        for(int i =0; i<n-(sl-1);i++){
-            boolean temp = reply(s.substring(i,i+sl), sl);
-            if(temp == true){
+        int sl = 3;
+        for(int r =0; r<s.length(); r++){
+            char ch = s.charAt(r);
+            hm.put(ch, hm.getOrDefault(ch,0)+1);
+            if(r-l==sl){
+                char tch = s.charAt(l);
+                hm.put(tch, hm.get(tch)-1);
+                if(hm.get(tch)==0){
+                    hm.remove(tch);
+                }
+                l++;
+            }
+            if(hm.size()==sl){
                 ans++;
             }
         }
