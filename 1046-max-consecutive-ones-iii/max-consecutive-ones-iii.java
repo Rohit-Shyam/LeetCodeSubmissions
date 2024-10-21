910989832e442990c2ebf338e2c36868ef8a84a9
Rohit Shyam <76948811+Rohit-Shyam@users.noreply.github.com>
2024-10-21 17:23:50 -0500

    Time: 4 ms (28.96%) | Memory: 47.4 MB (11.28%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -11,7 +11,7 @@ class Solution {
                 if(nums[l]==0){
                     temp--;
                     l=l+1;
-                    continue;
+                    break;
                 }
                 l++;
             }
